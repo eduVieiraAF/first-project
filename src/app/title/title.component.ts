a6991b512a4552dcc22a808cced16b94394b02d3
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-27 17:16:45 -0300

    ngOnChanges

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
-  public title: string = 'Welcome';
+export class TitleComponent implements OnInit, OnChanges {
+
+  @Input() public title: string = 'Welcome';
+
+
+  ngOnInit(): void {
+    // throw new Error('Function not implemented.');
+  }
+
+  ngOnChanges(): void {
+    console.log(`${this.title} after changes`);
+  }
 }
