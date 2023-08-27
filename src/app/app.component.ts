6619f461a5aef407ca986037c35fa79256765893
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-27 19:40:09 -0300

    onDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,46 @@ import { Component, OnInit,  DoCheck, AfterContentInit, AfterContentChecked, Aft
 @Component({
   selector: 'app-root',
   template: `
-    <app-title></app-title>
-    {{value}}
-    <button (click)="add()">Add</button>
+    <app-title title="Eduardo" *ngIf="destroy"></app-title>
+    <!-- {{value}}
+    <button (click)="add()">Add</button> -->
+    <br>
+    <button (click)="destroyComponent()">Destroy component</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
-  public value: number = 1;
+export class AppComponent implements OnInit, OnDestroy {
+  // public value: number = 1;
 
-  public add(): number {
-    return this.value += 1;
+  // public add(): number {
+  //   return this.value += 1;
+  // }
+
+  public destroy: boolean = true
+
+  destroyComponent() {
+    this.destroy = false
   }
 
   constructor() {
 
   }
 
-  ngDoCheck(): void {
-    console.log('ngDoCheck')
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit')
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked')
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit')
-  }
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked')
-  }
+  // ngDoCheck(): void {
+  //   console.log('ngDoCheck')
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log('ngAfterContentInit')
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log('ngAfterContentChecked')
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log('ngAfterViewInit')
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log('ngAfterViewChecked')
+  // }
   ngOnDestroy(): void {
 
   }
