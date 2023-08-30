fd01ce4d30db6169d9c8ff733e940d59354448ed
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-08-30 19:50:02 -0300

    ngFor

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -15,9 +15,25 @@ export class DirectivesComponent implements OnInit {
   }
   public condition: boolean = true
   public conditionClick: boolean = true
+  items: string[] = ['mango', 'banana', 'pear', 'apple', 'orange', 'strawberry', 'grape']
+  list: Array<{ name: string, age: number }> = [
+    { name: "Eduardo", age: 41 },
+    { name: "Daniel", age: 21 },
+    { name: "Sofia", age: 9 },
+  ]
 
   clicked() {
     if (this.conditionClick) this.conditionClick = false
     else this.conditionClick = true
+  }
+
+  addToList() {
+    this.list.push({ name: "LDI", age: 27 })
+    this.list.sort((a, b) => a.age - b.age)
+  }
+
+  clickEvent(i: number) {
+    console.log(i)
+    this.list.splice(i, 1)
   }
 }
